c6b445883c1174372d483f6a1730c31ea736ce8f
11905ThePack <32345853+11905ThePack@users.noreply.github.com>
2020-01-18 11:25:07 -0800

    SairaProgramCommit 1/18/20

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyTeleOp.java
@@ -44,7 +44,7 @@ public class SkyTeleOp extends OpMode {
     private double Soft = 0;
 
     private boolean Flipout= false;
-   // saira 1/18 11:00 am xxx
+   // saira 1/18 11:24 am
 
     @Override
     public void init() {
